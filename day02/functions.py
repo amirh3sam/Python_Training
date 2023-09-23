9d44647d67bd28a357c3fd4e646e6e174af24de9
Amirh3samB <amirh3sam.badiei@gmail.com>
2023-09-22 22:36:17 -0400

    update and add note day02

--- day02/functions.py
+++ day02/functions.py
@@ -10,7 +10,8 @@ Java Methods:
 import numbers
 
 
-def display_message():
+# generic type of function used as void or return function
+def display_message():  # is not retuning any value
     print('Hello Cydeo')
     print('Hello World')
 
@@ -18,21 +19,30 @@ def display_message():
 display_message()
 
 
-def value():
+def value():  # this is return function but not permanent return function because you can add the return or not
     return 'Python Programming'
 
 
+# you can assign it to the variable or pass it in to a print statement
 print(value())
 
 
+# this is not a generic function is for return function and should return int
 def return_int() -> int:
     return 10.0
 
 
+# on above 10.0 is float, but we ask for int if was java we get compile error
+# paython give you warning, but it will run the code just tell you ,should be int you provide float
 print(return_int())
 
 
-def square(num: int):
+# def square(num): no data type define in here
+#   return num * num
+# print(square(10))
+
+
+def square(num: int):  # we want to make sure num is int
     return num * num
 
 
@@ -41,14 +51,26 @@ print(square(10))
 
 # print( square('Java'))
 
-# print( divide('C#', 'Python'))
+# print( divide('C#', 'Python')) # get error because not be able to divide two strings
+# ----------------------------------------------------
+def add2(num1: int, num2: int):  # this function said had two argument and both should be int
+    return num1 + num2
 
+
+def add3(num1: int, num2: int) -> int:  # this function said had two argument and both should be int and return type
+    # should be int too
+    return num1 + num2
+
+
+# --------------------------------------------
+
+# there are "class numbers" that you need to import and called "numbers" you can add any numbers in this function now
 def add(num1: numbers, num2: numbers) -> numbers:
     return num1 + num2
 
 
+# you can pass int or float and return you int or float
 print(add(10, 20))
-
 print(add(10.5, 20.5))
 
 print('------------------------------------------')
@@ -88,12 +110,20 @@ print(sum(10, 20, 30))
 print(sum(10, 20, 30, 40))
 
 
-class test:
+class test:  # method must create in class and has space before def
     def method(self):
         pass
 
 
 print('----------------------------------------')
+
+
+# one string and 4 more with any datatype
+# just display the concatenation the result
+# if we don't give c and d and e ='' empty(SET DEFAULT VALUE) we are going to get error
+# so what we do is we give default arg so
+# also arg has to give to all of them in order
+# for example if you give default arg to third one your getting error on 4 and 5th one
 
 
 def concat(a: str, b, c='', d='', e=''):
@@ -104,7 +134,6 @@ concat('Cydeo', 'School')
 concat('Python', 3, 2.5)
 concat('Python', 3, 2.5, True)
 concat('Python', 3, 2.5, True, False)
-
 
 """
 1. Declaring
--- day02/strings.py
+++ day02/strings.py
@@ -1,18 +1,20 @@
 name = 'Python'
 
-print(len(name))
-print(name[0])
+print(len(name))  # len give you length for string
+print(name[0])  # to access to any index just put it inside []
 print(name[len(name) - 1])
 
-print(name[-1])
-print(name[-2])
+print(name[-1])  # to access last index number
+print(name[-2])  # to access the second last index number
 
 s = 'Java Programming Language'
-s2 = s[5:16]
-
+# if we want to get Programming P is index 5 and g is on index 15
+# to have g in our slice, we have to choose the next index that is 16 like java
+s2 = s[5:16]  # --> Slicing is like substring in java [start:end]
+# --> or you can do [start:] or [:end]
 print(s2)
 
-s3 = s[:4]
+s3 = s[:4]  #
 
 print(s3)
 
@@ -20,6 +22,7 @@ s4 = s[::-1]  # reverses the string after slicing
 
 print(s4)
 
+# Reverse method
 s = 'Python Programming'
 
 s5 = str(reversed(s))
@@ -35,10 +38,9 @@ print(s5)
 
 s5 = 'python Programming'
 
-s6 = s5[7:]  # by default it slices the string from index 7 to the last character
+s6 = s5[7:]  # by default, it slices the string from index 7 to the last character
 
 print(s6)
-
 
 s7 = 'CYDEO SCHOOL'
 # str(reversed(s7))
@@ -52,64 +54,61 @@ print('----------------------')
 
 s = 'python programming language'
 
-# s = s.capitalize()
-s = s.title()
+# s = s.capitalize() # only first character upper case
+s = s.title()   # all first characters of the sentence upper case
 print(s)
 
 s = "            Python           "
 s = s.strip()  # trim method of java
 
-print(s)
+# we have 2 more lstrip and rstrip that remove only one side(left or right)
 
+print(s)
 
 s = 'JAVA'
 
-print( s.index('A'))
-print(s.rindex('A'))
+print(s.index('A'))  # first index number like the same as Java
+print(s.rindex('A'))  # last index number of character like Java
 
 s = 'Java Java'
 
-s = s.replace('Java', 'Python', 1)
-
+s = s.replace('Java', 'Python', 1) # like java, replace Python with Java
+# that count 1 is only replace one Python with Java
 print(s)
 
 s = 'C# C# Python'
-
+# if we want to replace the second one, we add space before C# to make it uniq
 s = s.replace(' C#', ' Java')
 print(s)
 
-
 s = 'Java jAVA java JAVA Python Python'
-
+# how many 'java' with have in these sentences
+# lower is for ignoring the case sententiously
 count_java = s.lower().count('java')
 count_python = s.count('Python')
 
 print(count_java)
 print(count_python)
 
-
 s1 = 'java'
 s2 = 'JAVA'
-
-print( s1.lower() == s2.lower())  # ignore case
-
+# use == to compare the content of the two objects, so you need to
+# use a lower or upper case then compare them
+print(s1.lower() == s2.lower())  # ignore case
 
 s = 'Java'
 
-print(s[0].islower())
-print(s[0].isupper() )
-
+print(s[0].islower())  # boolean answer --> False
+print(s[0].isupper()) # boolean answer--> True
 
 s = 'a'
 
-print(s.isalpha())
+print(s.isalpha())  # boolean answer--> True
 
 s = '1'
 
-print(s.isdigit())
-
+print(s.isdigit())  # boolean answer--> False
 
 s = 'Cydeo School'
 
-print(s.istitle())
-
+print(s.istitle())   # boolean answer--> True
